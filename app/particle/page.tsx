69cfffefe6fa454ed40618be071643778e83d534
Ding Fan <ouud@fastmail.com>
2025-12-10 01:41:19 +0900

    add: create particle animation page with dual-direction particle groups

---
+++ app/particle/page.tsx
@@ -0,0 +1,9 @@
+import Particle from '@/components/Particle';
+
+export default function ParticlePage() {
+  return (
+    <main className="flex flex-1 flex-col w-full h-screen">
+      <Particle />
+    </main>
+  );
+}
